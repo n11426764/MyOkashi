1cdd27befb816d93b6f54ce2b7640368827fc9db
n11426764 <n11426764@qut.edu.au>
2024-12-05 12:05:36 +1000

    Initial Commit

---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by Masahiro Yasuda on 5/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
