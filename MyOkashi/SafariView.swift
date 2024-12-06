f17044d5e7fc0a19272ef2163442a9fe89cb1ef8
n11426764 <132448600+n11426764@users.noreply.github.com>
2024-12-07 07:47:08 +1000

    Recovered SafariView.swift

--- MyOkashi/SafariView.swift
+++ MyOkashi/SafariView.swift
@@ -6,13 +6,21 @@
 //
 
 import SwiftUI
+import SafariServices
 
-struct SafariView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// SFSafariViewControllerを起動する構造体
+struct SafariView: UIViewControllerRepresentable {
+    // 表示するホームページのURLを受け取る変数
+    let url: URL
+
+    // 表示するViewを生成するときに実行
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        // Safariを起動
+        return SFSafariViewController(url: url)
     }
-}
 
-#Preview {
-    SafariView()
+    // Viewが更新されたときに実行
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+        // 処理なし
+    }
 }
