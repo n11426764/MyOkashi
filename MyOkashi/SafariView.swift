a2978f9198c3f33cf15e6dc3e151000f852ac694
n11426764 <n11426764@qut.edu.au>
2024-12-07 06:13:29 +1000

    Added List View and Safari View

---
+++ MyOkashi/SafariView.swift
@@ -0,0 +1,18 @@
+//
+//  SafariView.swift
+//  MyOkashi
+//
+//  Created by Masahiro Yasuda on 7/12/2024.
+//
+
+import SwiftUI
+
+struct SafariView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafariView()
+}
